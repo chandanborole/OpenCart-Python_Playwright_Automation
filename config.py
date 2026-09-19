06d6d11e6c3067565ec33c9a1348aa5ef5b4a030
Chandan Borole <chandanborole@gmail.com>
2026-09-19 18:17:07 +0530

    config

---
+++ config.py
@@ -0,0 +1,8 @@
+class Config:
+    url = "https://tutorialsninja.com/demo"
+    first_name = "qa"
+    last_name = "automation"
+    email = "qaautomation@gmail.com"
+    password = "QRX@Xa6kQTDDSi"
+    confirm_password = "QRX@Xa6kQTDDSi"
+    telephone = "1234567890"
