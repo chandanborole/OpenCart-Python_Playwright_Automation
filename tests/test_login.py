9f88c8ceb0a84dc70731bd282ad0774e1fe7177b
Chandan Borole <chandanborole@gmail.com>
2026-09-20 14:55:43 +0530

    test_login

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,7 +4,7 @@ from OpenCart.config import Config
 
 # To verify new user registration
 
-def test_login(page:Page):
+def test_valid_login(page:Page):
 
     page.goto("https://tutorialsninja.com/demo/")
 
@@ -19,4 +19,21 @@ def test_login(page:Page):
     # Fill User ID / Password
     login_page.user_email_address(config_data.email)
     login_page.user_password(config_data.password)
+    login_page.click_login_button()
+
+def test_invalid_login(page:Page):
+
+    page.goto("https://tutorialsninja.com/demo/")
+
+    # Create Page Object
+    login_page = Login(page)
+    config_data = Config
+
+    # Navigate to login page
+    login_page.click_myaccount()
+    login_page.click_login_link()
+
+    # Fill User ID / Password
+    login_page.user_email_address(config_data.invalid_email)
+    login_page.user_password(config_data.invalid_password)
     login_page.click_login_button()
