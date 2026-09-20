0aa85093d0484502b2671957b8bb882f81e673eb
Chandan Borole <chandanborole@gmail.com>
2026-09-20 10:29:19 +0530

    config

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 class Config:
+
+    # Input Parameters for testcases / pages
+
+    # Register Page
     url = "https://tutorialsninja.com/demo"
     first_name = "qa"
     last_name = "automation"
@@ -6,3 +10,11 @@ class Config:
     password = "QRX@Xa6kQTDDSi"
     confirm_password = "QRX@Xa6kQTDDSi"
     telephone = "1234567890"
+
+    # Edit Your Account Information Page
+    edit_firstname = "automationqa"
+    edit_lastname = "pythonplaywright"
+    edit_telephone = "0987654321"
+
+    # Forgot Password
+    email_get_by_label = "qaautomation@gmail.com"
